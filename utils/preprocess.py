0ed8b3d70c7dcc4c7c3f391b306be2c9b2047d71
hyeri <hyerian96@gmail.com>
2022-01-27 11:07:35 +0100

    preprocess function

---
+++ utils/preprocess.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def preprocess(x):
+    mean = [129.4432, 129.4432, 129.4432]
+    std = [64.87448751, 64.87448751, 64.87448751]
+    # ensure image format
+    x = np.array(x, dtype='float32')
+    # normalize
+    x[..., 0] -= mean[0]
+    x[..., 1] -= mean[1]
+    x[..., 2] -= mean[2]
+    if std is not None:
+        x[..., 0] /= std[0]
+        x[..., 1] /= std[1]
+        x[..., 2] /= std[2]
+    return x
