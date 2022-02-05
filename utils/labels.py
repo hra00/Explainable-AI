af88c595cd62d7f3e25448fc94763f695291aa76
hyeri <hyerian96@gmail.com>
2022-02-05 15:27:04 +0100

    labels : biased_models added

--- utils/labels.py
+++ utils/labels.py
@@ -9,6 +9,10 @@ def get_labels(model_name):
         return emotion_labels_FERplus_impr
     elif model_name == 'RAF-impr-std_0124-1008_weights.h5':
         return emotion_labels_RAF_impr
+    elif model_name == 'RAF-bias-female_0203-1408_weights.h5':
+        return emotion_labels_RAF_impr
+    elif model_name == 'RAF-bias-male_0203-1144_weights.h5':
+        return emotion_labels_RAF_impr
     elif model_name == 'model_raf_h.h5':
         return emotion_labels_raf_h
     elif model_name == 'model_ferplus_h.h5':
